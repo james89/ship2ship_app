4ecf83ad04ae40f36ddb711884002e876409e262
Travis Vander Hoop <vanderhoop@me.com>
2013-11-06 09:56:00 -0500

    fixed merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
 
-<<<<<<< HEAD
-  # Marp Darp. Dparkasdfk asd
-=======
-  # Marp Darp.
-  #hihihihiihi
->>>>>>> 7460f7b72becad735681d750a07cef94d446f7c7
+
+  Marp Darp. This is the preferred text.
+
   protect_from_forgery
 end
